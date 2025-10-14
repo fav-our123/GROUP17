e8d40d6ffa325b8d58fed455d39229d4cfa2c73c
fav-our123 <fav.oured1405@gmail.com>
2025-10-14 21:51:11 +0100

    Fix backend

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.postgresql,//csc_dept_user:cXR49dWsnFEzszsRaDOrthcVmNuLxKud@dpg-d3jr85ur433s739i5a2g-a.oregon-postgres.render.com/csc_dept;
   ssl: {
     rejectUnauthorized: false // Required for Render's managed PostgreSQL
   }
